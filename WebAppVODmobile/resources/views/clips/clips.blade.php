aa8dcd598cda16afe4fbae7270ef6d93efd8a64a
elio_tohme <eliotohme@gmail.com>
2017-05-18 22:25:34 +0300

    bug fix

--- WebAppVODmobile/resources/views/clips/clips.blade.php
+++ WebAppVODmobile/resources/views/clips/clips.blade.php
@@ -55,6 +55,47 @@
 	  
 	</div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="AddClip_custom_modal" role="dialog">
+	<div class="modal-dialog modal-lg">
+	  <!-- Modal content-->
+	  <div class="modal-content">
+	    <div class="modal-header">
+	      <button type="button" class="close" data-dismiss="modal">&times;</button>
+	      <h4 class="modal-title">Add a Clip </h4>
+	    </div>
+	    <div class="modal-body">
+				<form action="{{url('/customclips')}}" method="post"  class="form-horizontal" role="form" name="form_addCustomClip">
+						{{ csrf_field() }}
+					<input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+					<div class="form-group">
+						<label for="Title" class="col-sm-1 control-label">Title</label>
+						<div class="col-sm-11">
+							<input class="form-control" id="Title" name="Title" type="text" placeholder="Title" required>
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="Stream" class="col-sm-1 control-label">Stream</label>
+						<div class="col-sm-5">
+							<input class="form-control" id="Stream" name="Stream" type="file" placeholder="Stream" required>
+						</div>
+						<label for="subtitle" class="col-sm-1 control-label">Subtitle</label>
+						<div class="col-sm-5">
+							<input class="form-control" id="Subtitle2" name="subtitle" type="file" placeholder="subtitle location" >
+						</div>
+					</div>
+				</form>
+	    </div>
+	    <div class="modal-footer">
+	    	<img hidden="true" class="loadingif" src=" {{url('/images/ajax-loader.gif')}} ">
+      		<button artist_id="{{ $artist->id }}" type="button" id ="btn_addCustomClip" class="btn btn-primary">Add</button>
+	    </div>
+	  </div>
+	  
+	</div>
+</div>
+
 <!-- Update Modal -->
 <div class="modal fade" id="UpdateClip_custom_modal" role="dialog">
 	<div class="modal-dialog modal-lg">
@@ -109,48 +150,6 @@
 	  </div>
 	</div>
 </div>
-
-
-<!-- Modal -->
-<div class="modal fade" id="AddClip_custom_modal" role="dialog">
-	<div class="modal-dialog modal-lg">
-	  <!-- Modal content-->
-	  <div class="modal-content">
-	    <div class="modal-header">
-	      <button type="button" class="close" data-dismiss="modal">&times;</button>
-	      <h4 class="modal-title">Add a Clip </h4>
-	    </div>
-	    <div class="modal-body">
-				<form action="{{url('/customclips')}}" method="post"  class="form-horizontal" role="form" name="form_addCustomClip">
-						{{ csrf_field() }}
-					<input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-					<div class="form-group">
-						<label for="Title" class="col-sm-1 control-label">Title</label>
-						<div class="col-sm-11">
-							<input class="form-control" id="Title" name="Title" type="text" placeholder="Title" required>
-						</div>
-					</div>
-					<div class="form-group">
-						<label for="Stream" class="col-sm-1 control-label">Stream</label>
-						<div class="col-sm-5">
-							<input class="form-control" id="Stream" name="Stream" type="file" placeholder="Stream" required>
-						</div>
-						<label for="subtitle" class="col-sm-1 control-label">Subtitle</label>
-						<div class="col-sm-5">
-							<input class="form-control" id="Subtitle2" name="subtitle" type="file" placeholder="subtitle location" >
-						</div>
-					</div>
-				</form>
-	    </div>
-	    <div class="modal-footer">
-	    	<img hidden="true" class="loadingif" src=" {{url('/images/ajax-loader.gif')}} ">
-      		<button artist_id="{{ $artist->id }}" type="button" id ="btn_addCustomClip" class="btn btn-primary">Add</button>
-	    </div>
-	  </div>
-	  
-	</div>
-</div>
-
 
 <link href="{{url('/css/select2.min.css')}}" rel="stylesheet" />
 <script src="{{url('/js/select2.min.js')}}"></script>
